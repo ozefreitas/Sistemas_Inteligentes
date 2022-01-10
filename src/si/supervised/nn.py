2781f183178c71f2a44eee3281f6f9117140b323
pg42872 <pg42872@alunos.uminho.pt>
2022-01-10 16:58:47 +0000

    Changes

--- src/si/supervised/nn.py
+++ src/si/supervised/nn.py
@@ -110,10 +110,10 @@ class NN(Model):
 class Flatten(Layer):
     def forward(self,input):
         self.input_shape= input.shape
-        output = input.reshape(input.shape[0],-1)
+        output = input.reshape(input.shape[0], -1)
         return output
 
-    def backward(self,output_error, lr):
+    def backward(self, output_error, lr):
         return output_error.reshape(self.input_shape)
 
 
@@ -169,3 +169,39 @@ class Conv2D(Layer):
         self.bias -= learning_rate*db
 
         return input_error
+
+# fazer pol
+
+
+class Poling2D(Model):
+    def __init__(self, size, stride):
+        self.size = size
+        self.stride = stride
+
+    def pool(self):
+        pass
+
+    def forward(self, input):
+        self.X_shape = input.shape
+        n, h, w, d = input.shape  # comprimento, altura e numero das imagens
+        h_out = (h - self.size) / self.stride + 1
+        w_out = (w - self.size) / self.stride + 1
+        if not w_out.is_intiger() or not h_out.is_intiger():
+            raise Exception("Invalid output dimension")
+        h_out, w_out = int(h_out), int(w_out)
+        X_reshaped = input.reshape()
+        self.X_col = im2col(X_reshaped, self.size, pad=0, stride=self.stride)
+
+        out, self.max_idx = self.pool(self.X_col)
+        out = out.reshape(h_out, w_out, n, d)
+        out = out.transpose(3, 2, 0, 1)
+        return out
+
+    def backward(self, output_error, learning_rate):
+        n, w, h, d = self.X_shape
+        dX_col = np.zeros_like(self.X_col)
+        dout_col = output_error.transpose(2, 3, 0, 1).ravel()
+        dX = self.dpool(dX_col, dout_col, self.max_idx)
+        dX = col2im(dX_col, (n*d, h, w, 1), self.size, pad=0, stride=self.stride)
+        dX = dX.reshape(self.X_shape)
+        return dX
